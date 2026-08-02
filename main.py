cef15394a0cb271854c94c9a2bde3246df2d070f
Daniil <danya.pogozhiy@mail.ru>
2026-08-03 02:06:05 +0300

    .

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from api.v1.auth import router
+from api.v1.auth import router as user_registration
 from fastapi import FastAPI
 from core.exceptions import register_exception_handlers
 
 app = FastAPI()
 
 def main():
-    app.include_router(router)
+    app.include_router(user_registration)
     register_exception_handlers(app)
 
 if __name__ == '__main__':
