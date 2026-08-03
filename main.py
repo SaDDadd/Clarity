b48ece394594e2059a3469e0cc9af049bf7d0b42
Daniil <danya.pogozhiy@mail.ru>
2026-08-03 11:37:07 +0300

    auto run main

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from api.v1.auth import router
 from fastapi import FastAPI
 from core.exceptions import register_exception_handlers
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(router)
 register_exception_handlers(app)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app')
