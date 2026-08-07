18da580f4ebeac799dc4a3f3c4610c51437aec68
Daniil <danya.pogozhiy@mail.ru>
2026-08-07 20:26:39 +0300

    .

--- alembic/versions/5176abe5d497_.py
+++ alembic/versions/5176abe5d497_.py
@@ -1,8 +1,8 @@
-"""initial
+"""empty message
 
-Revision ID: caaeabfcf233
+Revision ID: 5176abe5d497
 Revises: 
-Create Date: 2026-08-03 11:12:57.424753
+Create Date: 2026-08-07 20:13:03.047670
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'caaeabfcf233'
+revision: str = '5176abe5d497'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
