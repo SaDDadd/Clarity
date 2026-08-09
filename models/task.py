b2c7b1c4d0d02e45ddcb04e7656537e578ec9c4c
Daniil <danya.pogozhiy@mail.ru>
2026-08-09 16:56:38 +0300

    update task model

--- models/task.py
+++ models/task.py
@@ -11,7 +11,7 @@ class TaskModel(Base):
     task_description: Mapped[str] = mapped_column(Text, nullable=True)
     task_status: Mapped[str] = mapped_column(Enum('pending', 'in_progress', 'completed'), \
                                      nullable=True, server_default='pending')
-    project_id: Mapped[int] = mapped_column(ForeignKey('projects.project_id'), ondelete='CASCADE', nullable=False)
+    project_id: Mapped[int] = mapped_column(ForeignKey('projects.project_id', ondelete='CASCADE'), nullable=False)
     assigned_to: Mapped[int] = mapped_column(ForeignKey('users.user_id'), nullable=True)
     deadline: Mapped[datetime.date] = mapped_column(nullable=True)
     created_date: Mapped[datetime.datetime] = mapped_column(nullable=False, server_default=func.now())
