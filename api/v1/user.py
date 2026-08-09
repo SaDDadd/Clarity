a21b35a8b14a72491e285259d49c26518c667c9c
Daniil <danya.pogozhiy@mail.ru>
2026-08-09 18:14:17 +0300

    .

--- api/v1/user.py
+++ api/v1/user.py
@@ -8,10 +8,10 @@ from schemas.user import UpdateEmailRequest, UpdateUsernameRequest
 router = APIRouter()
 
 # PUT
-@router.put('/profile', tags=['Изменения профиля'], summary='Обновить имя пользователя') # Обновление имени профиля
-async def update_username(username: str, current_user: UserModel = Depends(current_user), db: AsyncSession = Depends(get_db)):
-    return await update_user_username(db, username, current_user.user_id)
+@router.put('/profile/username', tags=['Изменения профиля'], summary='Обновить имя пользователя') # Обновление имени профиля
+async def update_username(request: UpdateUsernameRequest, current_user: UserModel = Depends(current_user), db: AsyncSession = Depends(get_db)):
+    return await update_user_username(db, request.username, current_user.user_id)
 
-@router.put('/profile', tags=['Изменения профиля'], summary='Обновить email пользователя') # Обновление email профиля
-async def update_email(email: str, current_user: UserModel = Depends(current_user), db: AsyncSession = Depends(get_db)):
-    return await update_user_email(db, email, current_user.user_id)
+@router.put('/profile/email', tags=['Изменения профиля'], summary='Обновить email пользователя') # Обновление email профиля
+async def update_email(request: UpdateEmailRequest, current_user: UserModel = Depends(current_user), db: AsyncSession = Depends(get_db)):
+    return await update_user_email(db, request.email, current_user.user_id)
--- services/user_service.py
+++ services/user_service.py
@@ -5,9 +5,9 @@ from repositories.user_repository import UserRepository
 async def update_user_username(db, username: str, current_user_id: int):
     repo = UserRepository(db)
     if len(username) == 0 or len(username) > 50:
-        raise LackOfInformationException()
+        raise LackOfInformationException('Имя не может быть пустым или превышать 50 символов!')
     if await repo.check_user_exists_by_username(username):
-        raise ConflictException('')
+        raise ConflictException('Пользователь с таким именем уже существует!')
     if await repo.update_username(current_user_id, username):
         return {'message': 'Имя пользователя обновлено!'}
     else:
@@ -16,9 +16,9 @@ async def update_user_username(db, username: str, current_user_id: int):
 async def update_user_email(db, email: str, current_user_id: int):
     repo = UserRepository(db)
     if len(email) == 0 or len(email) > 100:
-        raise LackOfInformationException()
+        raise LackOfInformationException('Email не может быть пустым или превышать 100 символов!')
     if await repo.check_user_exists_by_email(email):
-        raise ConflictException('')
+        raise ConflictException('Пользователь с таким email уже существует!')
     if await repo.update_email(current_user_id, email):
         return {'message': 'Email пользователя обновлено!'}
     else:
