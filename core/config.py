4d6b50c7cb900037f4569b6796e6a44609314ab4
Daniil <danya.pogozhiy@mail.ru>
2026-08-11 13:42:00 +0300

    the password from core is hidden

--- core/config.py
+++ core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     DB_HOST: str = 'localhost'
     DB_PORT: int = 3306
     DB_USER: str = 'root'
-    DB_PASSWORD: str = '2007'
+    DB_PASSWORD: str = '${DB_PASSWORD}'
     DB_NAME: str = 'task_to_do'
     DB_DRIVER: str = 'aiomysql'
 
