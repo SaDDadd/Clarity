7f47dd934e6633833082d5b1c3a1907dd42478ac
Daniil <danya.pogozhiy@mail.ru>
2026-08-13 19:57:17 +0300

    start work with pytest

--- core/config.py
+++ core/config.py
@@ -33,4 +33,35 @@ class Settings(BaseSettings):
         extra='ignore'
     )
 
+class SettingsTEST(BaseSettings):
+    DB_HOST: str = 'localhost'
+    DB_PORT: int = 3306
+    DB_USER: str = 'root'
+    DB_PASSWORD: str = '${DB_PASSWORD}'
+    DB_NAME: str = 'task_to_do_test'
+    DB_DRIVER: str = 'aiomysql'
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"mysql+{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    JWT_SECRET_KEY: str = Field(..., env="JWT_SECRET_KEY")
+    JWT_ALGORITHM: str = 'HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    # ВАЖНО: используем default_factory=list, а не просто []
+    CORS_ORIGINS: str = ''
+
+    @property
+    def cors_origins_list(self) -> List[str]:
+        if not self.CORS_ORIGINS:
+            return []
+        return [item.strip() for item in self.CORS_ORIGINS.split(',') if item.strip()]
+
+    model_config = SettingsConfigDict(
+        env_file='.env.test',
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
+
+test_settings =SettingsTEST()
 settings = Settings()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,51 @@
 # клиент, база данных, тестовый пользователь, токен
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, \
+    AsyncSession
+from httpx import AsyncClient, ASGITransport
+from main import app
+from core.dependencies import get_db
+from alembic import command
+from core.config import test_settings
+from pathlib import Path
+from alembic.config import Config
+
+@pytest_asyncio.fixture(scope='session')
+async def engine():
+    AlembicConfig = Config(Path(__file__).parent.parent / 'alembic.ini') # Передача всех параметров из alembic.ini
+    AlembicConfig.set_main_option('sqlalchemy.url', test_settings.DATABASE_URL) # Переопределяем URL базы данных
+    command.upgrade(AlembicConfig, 'head') # Применение миграций
+    engine = create_async_engine(test_settings.DATABASE_URL) # Создание асинхронного движка SQLAlchemy
+    yield engine # Отдаем джижок тестам
+    await engine.dispose() # Закрываем движок
+    command.downgrade(AlembicConfig, 'base') # Откатываем миграции
+
+@pytest_asyncio.fixture(scope='function')
+async def db_session(engine):
+    session = None # На случай исключений
+    try:
+        session_factory = async_sessionmaker(bind=engine, expire_on_commit=False) # Создаем фабрику сессий привязанную к движку
+        session = session_factory() # Создание новой сессии
+        yield session # Отдаем сессию
+    finally:
+        if session is not None: # Если сессия была, откатываем транзакцию
+            await session.rollback()
+            await session.close()
+
+@pytest_asyncio.fixture(scope='function')
+async def async_client(db_session):
+    async def override_get_db(): # Функция подменяет get_db
+        return db_session
+    app.dependency_overrides[get_db] = override_get_db # Переопределяем зависимость get_db
+    async with AsyncClient(transport=ASGITransport(app=app), base_url='http://test') as client: # Создание клиента, который тестирует без запуска сервера
+        yield client # Отдаем клиент тесту
+    app.dependency_overrides.clear() # После теста очищаем переопределение
+
+@pytest_asyncio.fixture
+async def auth_headers():
+
+@pytest_asyncio.fixture
+async def test_project():
+
+@pytest_asyncio.fixture
+async def test_task():
