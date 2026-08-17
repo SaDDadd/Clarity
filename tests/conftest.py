cd7288ee5c780f1765d678d694a070c1a492b37d
Daniil <danya.pogozhiy@mail.ru>
2026-08-17 21:52:11 +0300

    Refactor tests to use fixtures for setup and teardown, improving code organization and readability.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,14 @@ from alembic import command
 from core.config import test_settings
 from pathlib import Path
 from alembic.config import Config
+from models.user import UserModel
+from core.security import hash_password, create_access_token
+from repositories.user_repository import UserRepository
+from repositories.project_repository import ProjectRepository
+from repositories.task_repository import TaskRepository
+import uuid
+import time
+from schemas.task import TaskCreate
 
 @pytest_asyncio.fixture(scope='session')
 async def engine():
@@ -41,11 +49,32 @@ async def async_client(db_session):
         yield client # Отдаем клиент тесту
     app.dependency_overrides.clear() # После теста очищаем переопределение
 
-@pytest_asyncio.fixture
-async def auth_headers():
+@pytest_asyncio.fixture(scope='function')
+async def test_user(db_session):
+    repo = UserRepository(db_session)
+    hashed_password = hash_password('qwertyui')
+    timestamp = time.time_ns()
+    uui = uuid.uuid4()
+    user = await repo.create_user(f'testuser_<{timestamp}>', f'test_<{uui}>@mail.ru', hashed_password)
+    return user
 
-@pytest_asyncio.fixture
-async def test_project():
+@pytest_asyncio.fixture(scope='function')
+async def auth_headers(test_user):
+    token = create_access_token({'sub': test_user.user_id})
+    return {'Authorization': f'Bearer {token}'}
 
-@pytest_asyncio.fixture
-async def test_task():
+@pytest_asyncio.fixture(scope='function')
+async def test_project(db_session, test_user):
+    repo = ProjectRepository(db_session)
+    uui = uuid.uuid4()
+    project = await repo.create_project_with_admin(name=f'Test_project_<{uui}>', description=None, \
+                                                   admin_id=test_user.user_id, role='admin')
+    return project
+
+@pytest_asyncio.fixture(scope='function')
+async def test_task(db_session, test_project, test_user):
+    repo = TaskRepository(db_session)
+    create = TaskCreate(title='Test_task', task_description=None, task_status='pending', \
+                        assigned_to=None, deadline=None)
+    task = await repo.create_task(project_id=test_project.project_id, task=create)
+    return task
