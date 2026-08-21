cb32bbb8f26847397ed8f363888a305eb3fbacb3
Daniil <danya.pogozhiy@mail.ru>
2026-08-21 14:22:36 +0300

    feat: add ProjectInvitationModel to project_repository.py

--- repositories/project_repository.py
+++ repositories/project_repository.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.exceptions import ConflictException, LackOfInformationException, NotFoundException
 from models.project import ProjectModel
 from models.project_members import ProjectMemberModel
+from models.project_invitations import ProjectInvitationModel
 from models.task import TaskModel
 from sqlalchemy import func 
 
